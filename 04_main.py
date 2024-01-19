4d06a4a76830eece43533df32ab8e0b14924ffe6
Herman <122231001+hermanproskurnin@users.noreply.github.com>
2024-01-19 16:20:11 +0100

    Update 04_main.py

--- 04_main.py
+++ 04_main.py
@@ -1,57 +1,57 @@
+```python
 import telebot
 from config import keys, TOKEN
 from extensions import APIException, CryptoConverter, DeclensionByCases
 
 bot = telebot.TeleBot(TOKEN)
 
-# Приветсвие
+# Greeting
 @bot.message_handler(commands=['start'])
 def start(message):
-    text = f'Приветствую, {message.from_user.first_name}! \n\nМеня зовут Конвертор валют , и я могу:' \
-           f'\n- перевести Вашу валюту через команду <имя валюты> <в какую валюту перевести> ' \
-           f'<количество переводимой валюты> (через пробелы).' \
-           f'\nНапример: рубль евро 1\n' \
-           f'\n- показать валюты, которые Вы можете конвертировать через команду \n/values;' \
-           f'\n- напомнить, что я могу через команду \n/help.'
+    text = f"Welcome, {message.from_user.first_name}!\n\nI am the Currency Converter, and I can:" \
+           f"\n- convert your currency using the command <currency name> <target currency> " \
+           f"<amount of currency to convert> (separated by spaces)." \
+           f"\nFor example: ruble euro 1\n" \
+           f"\n- show the currencies you can convert using the command \n/values;" \
+           f"\n- remind you what I can do using the command \n/help."
     bot.send_message(message.chat.id, text)
 
-
-# Обрабатываеи команду help:
+# Handling the help command:
 @bot.message_handler(commands=['help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы перевести валюту, напишите мне команду следующим образом:' \
-           '\n<имя валюты> <в какую валюту перевести> <количество переводимой валюты> через пробелы.' \
-           '\nНапример: рубль евро 1\n' \
-           '\nЧтобы увидеть валюты, которые я смогу конвертировать, введите команду\n/values'
+    text = 'To convert currency, write me a command like this:' \
+           '\n<currency name> <target currency> <amount of currency to convert> separated by spaces.' \
+           '\nFor example: ruble euro 1\n' \
+           '\nTo see the currencies I can convert, enter the command \n/values'
     bot.reply_to(message, text)
 
-# Список волют для перевода
+# List of currencies for conversion
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
-    text = 'Я умею переводить:'
+    text = 'I can convert:'
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
 
-# Ловим Exception
+# Catching Exception
 @bot.message_handler(content_types=['text', ])
 def get_price(message: telebot.types.Message):
     try:
         values = message.text.lower().split(' ')
 
         if len(values) != 3:
-            raise APIException('\nКоличество параметров не совпадает!\n'
-                                      '\nПишите, пожалуйста, только так:\n<имя валюты> ' \
-                                      '<в какую валюту перевести> <количество переводимой валюты> через пробелы'
-                                      '\nИначе я Вас не понимаю :(')
+            raise APIException('\nThe number of parameters does not match!\n'
+                               '\nPlease write like this:\n<currency name> ' \
+                               '<target currency> <amount of currency to convert> separated by spaces'
+                               '\nOtherwise, I do not understand you :(')
 
         quote, base, amount = values
         total_base = CryptoConverter.get_price(quote, base, amount)
     except APIException as e:
-        bot.reply_to(message, f'Ой Вы что-то не так написали:\n{e}')
+        bot.reply_to(message, f'Oops, you wrote something wrong:\n{e}')
     except Exception as e:
-        bot.reply_to(message, f'Что-то пошло не так :(\n\n{e}\n'
-                              f'\nНо это не Ваша вина, так просто сегодня такой день.')
+        bot.reply_to(message, f'Something went wrong :(\n\n{e}\n'
+                              f'\nBut it\'s not your fault, it\'s just one of those days.')
     else:
         inclined_quote = DeclensionByCases(quote, float(amount))
         inclined_base = DeclensionByCases(base, float(total_base))
